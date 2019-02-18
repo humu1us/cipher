18e8c9b686be2cc2028f5dc5ee500a5ca225730b
Felipe Ortiz <fortizc@gmail.com>
2019-02-18 16:29:55 -0300

    Add functions to encrypt and decrypt

---
+++ cipher/cipher.py
@@ -0,0 +1,36 @@
+import os
+import base64
+from Crypto.Cipher import AES
+import cipher.core.pkcs7 as pkcs7
+from cipher.core.derivation import derive_password
+
+HEADER = b"_GV_ENC_"
+
+
+def check_header(raw):
+    return raw[:8] == HEADER
+
+
+def encrypt(passwd, text):
+    padded = pkcs7.pad(text)
+    salt = os.urandom(8)
+    key, iv = derive_password(passwd, salt)
+    c = AES.new(key, AES.MODE_CBC, iv)
+    encrypted = base64.b64encode(HEADER + salt + c.encrypt(padded))
+
+    return base64.b64encode(encrypted).decode("utf-8")
+
+
+def decrypt(passwd, encrypted):
+    raw = base64.b64decode(base64.b64decode(encrypted))
+    if not check_header(raw):
+        raise RuntimeError("Missing header, cannot decrypt")
+
+    salt = raw[8:16]
+    key, iv = derive_password(passwd, salt)
+    encrypted = raw[16:]
+
+    c = AES.new(key, AES.MODE_CBC, iv)
+    text = pkcs7.unpad(c.decrypt(encrypted))
+
+    return text.decode("utf-8")
