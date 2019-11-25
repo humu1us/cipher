7e025a685f7736e12655de7e7869265fb1755132
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2019-11-25 00:56:05 -0300

    Update arguments
    Now, there are two subcommands: 'encrypt' and 'decrypt'. Both of them
    have the same arguments, with the '-i' and '-f' as mutually exclusive
    with at least one of them required. The same for the '-k' and '-s' ones.
    The '-o' argument is optional.

--- cipher/cli/ciphercmd.py
+++ cipher/cli/ciphercmd.py
@@ -1,47 +1,79 @@
 import argparse
-import os.path as path
+import os
+import sys
 import cipher
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-                 description="ciphercmd: encryption and decryption tool"
-            )
-
-    parser.add_argument("-f",
-                        "--file",
-                        dest="file",
-                        type=str,
-                        required=True,
-                        help="File to encrypt")
-
-    parser.add_argument("-k",
-                        "--key",
-                        dest="key",
-                        type=str,
-                        required=False,
-                        default=path.join(path.expanduser("~"), ".passwd"),
-                        help="Key used to encrypt the file")
-
-    parser.add_argument("-o",
-                        "--output",
-                        dest="out",
-                        type=str,
-                        required=False,
-                        default="file.out",
-                        help="File name for the result file")
-
-    parser.add_argument("-e",
-                        "--encrypt",
-                        dest="enc",
-                        action="store_true",
-                        help="Use ciphercmd in encryption mode")
-
-    parser.add_argument("-d",
-                        "--decrypt",
-                        dest="dec",
-                        action="store_true",
-                        help="Use ciphercmd in decryption mode")
+    description = "ciphercmd: encryption and decryption tool"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("-v",
+                        "--version",
+                        action="version",
+                        version=cipher.__version__)
+    mode = parser.add_subparsers()
+    encr = mode.add_parser("encrypt",
+                           description="encrypt",
+                           help="encryption mode")
+    encr.set_defaults(func=encrypt)
+    encr_gin = encr.add_mutually_exclusive_group(required=True)
+    encr_gkey = encr.add_mutually_exclusive_group(required=True)
+    decr = mode.add_parser("decrypt",
+                           description="decrypt",
+                           help="decryption mode")
+    decr.set_defaults(func=decrypt)
+    decr_gin = decr.add_mutually_exclusive_group(required=True)
+    decr_gkey = decr.add_mutually_exclusive_group(required=True)
+    optional_args = [
+        {
+            "args": ("-i",),
+            "kwargs": {
+                "dest": "input",
+                "help": "input to %s"
+            }
+        }, {
+            "args": ("-f",),
+            "kwargs": {
+                "dest": "file",
+                "help": "file to %s"
+            }
+        }, {
+            "args": ("-k",),
+            "kwargs": {
+                "dest": "key_file",
+                "help": "file with the key to %s"
+            }
+        }, {
+            "args": ("-s",),
+            "kwargs": {
+                "dest": "key_str",
+                "help": "key to %s"
+            }
+        }, {
+            "args": ("-o",),
+            "kwargs": {
+                "dest": "output",
+                "help": "%sed output file"
+            }
+        },
+    ]
+    subparsers = (encr, decr)
+    groups = {
+        "encrypt": [encr_gin, encr_gkey],
+        "decrypt": [decr_gin, decr_gkey]
+    }
+    for mode in subparsers:
+        desc = mode.description
+        for arg in optional_args:
+            args = arg["args"]
+            kwargs = arg["kwargs"].copy()
+            kwargs["help"] = kwargs["help"] % desc
+            if args[0] in ("-i", "-f"):
+                groups[desc][0].add_argument(*args, **kwargs)
+            elif args[0] in ("-k", "-s"):
+                groups[desc][1].add_argument(*args, **kwargs)
+            else:
+                mode.add_argument(*args, **kwargs)
 
     return parser
 
